730d122ede972ba5f11c30778dee97798c93bdd1
Stone Kim <Mon8Cats@gmail.com>
2020-11-10 01:07:41 -0600

    initial commit

---
+++ ParkyApi/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,21 @@
+﻿using ParkyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyApi.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int id);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark park);
+        bool UpdateNationalPark(NationalPark park);
+        bool DeleteNationalPark(NationalPark park);
+        bool Save();
+    }
+}
---
+++ ParkyApi/Repository/NationalParkRepository.cs
@@ -0,0 +1,63 @@
+﻿using ParkyApi.Data;
+using ParkyApi.Models;
+using ParkyApi.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyApi.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool CreateNationalPark(NationalPark park)
+        {
+            _db.NationalParks.Add(park);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark park)
+        {
+            _db.NationalParks.Remove(park);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int id)
+        {
+            return _db.NationalParks.FirstOrDefault(o => o.Id == id);
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return _db.NationalParks.OrderBy(o => o.Name).ToList();
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            return _db.NationalParks.Any(o => o.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NationalParks.Any(o => o.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateNationalPark(NationalPark park)
+        {
+            _db.NationalParks.Update(park);
+            return Save();
+        }
+    }
+}
